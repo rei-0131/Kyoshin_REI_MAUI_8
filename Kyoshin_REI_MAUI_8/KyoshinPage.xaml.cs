3ebeae1870b8fea7aaf67e67022f84256506051e
rei-0131 <yitengyuudai32@gmail.com>
2024-03-09 09:00:13 +0900

    0900_長周期地震動のモニターの追加と切り替えスイッチの追加

--- Kyoshin_REI_MAUI_8/KyoshinPage.xaml.cs
+++ Kyoshin_REI_MAUI_8/KyoshinPage.xaml.cs
@@ -1,12 +1,13 @@
 using KyoshinMonitorLib.UrlGenerator;
 using Microsoft.Maui.Graphics.Platform;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace Kyoshin_REI_MAUI_8;
 
 public partial class KyoshinPage : ContentPage
 {
-    public static int add_sc = 0;
+    public static int add_sc = -2;
 
 	public KyoshinPage()
 	{
@@ -29,13 +30,16 @@ public partial class KyoshinPage : ContentPage
         {
             if(Geoloc.app_window)
             {
-                var nowtime = DateTime.Now.AddSeconds(add_sc);
-                image_flo = ImageSource.FromUri(new Uri($"http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/jma_s/{nowtime.ToString("yyyyMMdd")}/{nowtime.ToString("yyyyMMddHHmmss")}.jma_s.gif"));
-
+                var targettime = DateTime.Now.AddSeconds(add_sc);
+                Debug.WriteLine(add_sc);
+                if (kyoshin_type.IsToggled)
+                    image_flo = ImageSource.FromUri(new Uri($"http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/jma_s/{targettime.ToString("yyyyMMdd")}/{targettime.ToString("yyyyMMddHHmmss")}.jma_s.gif"));
+                else
+                    image_flo = ImageSource.FromUri(new Uri($"https://www.lmoni.bosai.go.jp/monitor/data/data/map_img/RealTimeImg/abrspmx_s/{targettime.ToString("yyyyMMdd")}/{targettime.ToString("yyyyMMddHHmmss")}.abrspmx_s.gif"));
                 if (MainPage.result_eew != null && MainPage.result_eew.Data.Result.Message != "ÉfÅ[É^Ç™Ç†ÇËÇ‹ÇπÇÒ")
                 {
-                    ps_image_ = ImageSource.FromUri(new Uri($"http://www.kmoni.bosai.go.jp/data/map_img/PSWaveImg/eew/{nowtime.ToString("yyyyMMdd")}/{nowtime.ToString("yyyyMMddHHmmss")}.eew.gif"));
-                    fore_image_ = ImageSource.FromUri(new Uri($"http://www.kmoni.bosai.go.jp/data/map_img/EstShindoImg/eew/{nowtime.ToString("yyyyMMdd")}/{nowtime.ToString("yyyyMMddHHmmss")}.eew.gif"));
+                    ps_image_ = ImageSource.FromUri(new Uri($"http://www.kmoni.bosai.go.jp/data/map_img/PSWaveImg/eew/{targettime.ToString("yyyyMMdd")}/{targettime.ToString("yyyyMMddHHmmss")}.eew.gif"));
+                    fore_image_ = ImageSource.FromUri(new Uri($"http://www.kmoni.bosai.go.jp/data/map_img/EstShindoImg/eew/{targettime.ToString("yyyyMMdd")}/{targettime.ToString("yyyyMMddHHmmss")}.eew.gif"));
                 }
 
                 fore_image.Source = fore_image_;
@@ -50,13 +54,9 @@ public partial class KyoshinPage : ContentPage
     private void time_slider_ValueChanged(object sender, ValueChangedEventArgs e)
     {
         if (Convert.ToInt32(e.NewValue) == 0)
-        {
             add_sc = -2;
-        }
         else
-        {
             add_sc = Convert.ToInt32(e.NewValue) * -60;
-        }
         time_label.Text = $"{Convert.ToInt32(e.NewValue)}ï™";
     }
 }
